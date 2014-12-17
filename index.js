91f24e504892f872729885efc0c3884029242cc0
Dan Bucholtz <dan.bucholtz@gmail.com>
2014-12-17 16:25:41 -0600

    user api

---
+++ index.js
@@ -0,0 +1,61 @@
+var request = require('request');
+var Q = require("q");
+
+var utils = require("soa-example-core-utils");
+
+var config = require("soa-example-service-config").config();
+
+var getUserById = function(id, token){
+	var deferred = Q.defer();
+	
+	var url = utils.createBaseUrl(config.userServiceIp, config.userServicePort);
+	request.get(url + "/users/" + id, function(err, response, body){
+		if ( err ){
+			deferred.reject(err);
+		}
+		else{
+			deferred.resolve(body);
+		}
+	});
+
+	return deferred.promise;
+};
+
+var getUserByEmailAddress = function(emailAddress, token){
+	var deferred = Q.defer();
+	
+	var url = utils.createBaseUrl(config.userServiceIp, config.userServicePort);
+	request.get(url + "/users/" + emailAddress, function(err, response, body){
+		if ( err ){
+			deferred.reject(err);
+		}
+		else{
+			deferred.resolve(body);
+		}
+	});
+
+	return deferred.promise;
+};
+
+var getUsers = function(token){
+	var deferred = Q.defer();
+	
+	var url = utils.createBaseUrl(config.userServiceIp, config.userServicePort);
+	request.get(url + "/users", function(err, response, body){
+		if ( err ){
+			deferred.reject(err);
+		}
+		else{
+			deferred.resolve(body);
+		}
+	});
+
+	return deferred.promise;
+};
+
+
+module.exports = {
+	getUserById : getUserById,
+	getUserByEmailAddress : getUserByEmailAddress,
+	getUsers : getUsers
+}
