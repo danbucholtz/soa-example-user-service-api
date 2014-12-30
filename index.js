9a3b961187d081d015469c6d3e6d54a4b6ba4fb1
Dan Bucholtz <dan.bucholtz@gmail.com>
2014-12-30 15:38:32 -0600

    updates

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ var utils = require("soa-example-core-utils");
 
 var config = require("soa-example-service-config").config();
 
+var redisUtil = require('soa-example-redis-util');
+
 var createUser = function(emailAddress, password){
 	var deferred = Q.defer();
 
@@ -24,10 +26,19 @@ var createUser = function(emailAddress, password){
 
 var getUserById = function(id, token){
 	var deferred = Q.defer();
-	
-	var url = utils.createBaseUrl(config.userServiceIp, config.userServicePort);
-	utils.get(url + "/users/" + id).then(function(user){
-		deferred.resolve(user);
+
+	redisUtil.get(id).then(function(user){
+		if ( user ){
+			deferred.resolve(user);
+			return;
+		}
+
+		var url = utils.createBaseUrl(config.userServiceIp, config.userServicePort);
+		
+		utils.get(url + "/users/" + id).then(function(user){	
+			redisUtil.put(id, user);
+			deferred.resolve(user);
+		});
 	});
 
 	return deferred.promise;
@@ -36,10 +47,18 @@ var getUserById = function(id, token){
 var getUserByEmailAddress = function(emailAddress){
 	var deferred = Q.defer();
 
-	var url = utils.createBaseUrl(config.userServiceIp, config.userServicePort);
+	redisUtil.get(emailAddress).then(function(user){
+		if ( user ){
+			deferred.resolve(user);
+			return;
+		}
+
+		var url = utils.createBaseUrl(config.userServiceIp, config.userServicePort);
 	
-	utils.get(url + "/users/" + emailAddress).then(function(user){
-		deferred.resolve(user);
+		utils.get(url + "/users/" + emailAddress).then(function(user){
+			redisUtil.put(emailAddress, user);
+			deferred.resolve(user);
+		});
 	});
 
 	return deferred.promise;
@@ -47,13 +66,21 @@ var getUserByEmailAddress = function(emailAddress){
 
 var getUserByToken = function(token){
 	var deferred = Q.defer();
-	
-	var url = utils.createBaseUrl(config.userServiceIp, config.userServicePort);
-	
-	utils.get(url + "/users/accessToken/" + token).then(function(user){
-		deferred.resolve(user);
-	});
 
+	redisUtil.get(token).then(function(user){
+		if ( user ){
+			deferred.resolve(user);
+			return;
+		}
+
+		var url = utils.createBaseUrl(config.userServiceIp, config.userServicePort);
+	
+		utils.get(url + "/users/accessToken/" + token).then(function(user){
+			redisUtil.put(token, user);
+			deferred.resolve(user);
+		});
+	});
+	
 	return deferred.promise;
 };
 
